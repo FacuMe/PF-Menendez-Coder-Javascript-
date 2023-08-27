5aa654dc6a3763edfd7396c766b7d3f1f7021553
FacuM <facmenendez@gmail.com>
2023-08-27 20:52:12 -0300

    simulador de carrito

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-//navbar scrolled
+//Change navbar when scrolled
 const brandLogo = document.querySelector('.brand-logo');
 const brand = document.querySelector('.brand');
 const brandContainer = document.querySelector('.brand-container');
@@ -17,64 +17,7 @@ window.addEventListener('scroll', () => {
 });
 
 
-
-// Función para calcular el costo de envío
-function calcularCostoEnvio() {
-    let codigoPostal;
-    let costoEnvio;
-    let envioExpress;
-    let costoFinal;
-    
-    while (isNaN(codigoPostal) || codigoPostal < 1000 || codigoPostal > 2000) {
-        codigoPostal = Number(prompt("Por favor, ingresa tu código postal:"));
-        if (codigoPostal === 0) {
-            return;
-        }
-        else if (isNaN(codigoPostal) || codigoPostal < 1000 || codigoPostal > 2000) {
-            alert("Ingresa un código postal numérico válido (entre 1000-2000, sólo Buenos Aires y CABA)");
-        }
-    }
-
-    if (codigoPostal >= 1700 && codigoPostal <= 1800) {
-        costoEnvio = 0;
-    } 
-    else if ((codigoPostal >= 1600 && codigoPostal <= 1699) || (codigoPostal >= 1801 && codigoPostal <= 1900)) {
-        costoEnvio = 500;
-    }
-    else {
-        costoEnvio = 800;
-    }
-
-    envioExpress = calcularEnvioExpress(); 
-    costoFinal = costoEnvio + envioExpress;
-
-    alert("El costo de envío es $" + costoFinal + ".");
-}
-
-function calcularEnvioExpress () {
-    let valorEnvioExpress;
-    let envioExpress;
-
-    while (envioExpress !== 1 && envioExpress !== 2) {
-        envioExpress = Number(prompt("¿Quieres envío express? \n (Ingresa el número de opción) \n 1. Si. \n 2. No"));
-    }
-    if (envioExpress === 1) {
-        valorEnvioExpress = 1000;
-        return valorEnvioExpress;
-    }
-    else {
-        return 0;
-    }
-}
-  
-const botonCalcular = document.getElementById("boton-calcular-envio");
-botonCalcular.addEventListener("click", calcularCostoEnvio);
-
-
-
-
-
-//Carga y filtro de productos 
+// Carga de productos desde archivo JSON (TO DO)
 let listaDeProductos = [];
 document.addEventListener('DOMContentLoaded', () => {
     
@@ -134,7 +77,173 @@ function mostrarTodosLosProductos(productos) {
             });
 }
 
+// Filtro productos (TO DO)
 function filtrarPorTipo(listaDeProductos){
     let productosFiltradosPorTipo = listaDeProductos.filter(producto => producto.tipo === "Tinto");
     return productosFiltradosPorTipo;
 }
+
+
+// Función para calcular el costo de envío (PRIMERA PREENTREGA)
+function calcularCostoEnvio() {
+    let codigoPostal;
+    let costoEnvio;
+    let envioExpress;
+    let costoFinal;
+    
+    while (isNaN(codigoPostal) || codigoPostal < 1000 || codigoPostal > 2000) {
+        codigoPostal = Number(prompt("Por favor, ingresa tu código postal:"));
+        if (codigoPostal === 0) {
+            return;
+        }
+        else if (isNaN(codigoPostal) || codigoPostal < 1000 || codigoPostal > 2000) {
+            alert("Ingresa un código postal numérico válido (entre 1000-2000, sólo Buenos Aires y CABA)");
+        }
+    }
+
+    if (codigoPostal >= 1700 && codigoPostal <= 1800) {
+        costoEnvio = 0;
+    } 
+    else if ((codigoPostal >= 1600 && codigoPostal <= 1699) || (codigoPostal >= 1801 && codigoPostal <= 1900)) {
+        costoEnvio = 500;
+    }
+    else {
+        costoEnvio = 800;
+    }
+
+    alert("El costo de envío es $" + costoEnvio + ".");
+    envioExpress = calcularEnvioExpress(); 
+    costoFinalEnvio = costoEnvio + envioExpress;
+    return costoFinalEnvio;
+}
+
+function calcularEnvioExpress () {
+    let valorEnvioExpress;
+    let envioExpress;
+
+    while (envioExpress !== 1 && envioExpress !== 2) {
+        envioExpress = Number(prompt("¿Quieres envío express? \n (Costo $1.000 adicional) \n Ingresa el número de opción: \n 1. Si. \n 2. No"));
+    }
+    if (envioExpress === 1) {
+        valorEnvioExpress = 1000;
+        return valorEnvioExpress;
+    }
+    else {
+        return 0;
+    }
+}
+
+//Simular carrito (SEGUNDA PREENTREGA)
+class Producto{
+    constructor(id, nombre, tipo, precio, imagen, stock, cantidad){
+        this.id = id;
+        this.nombre = nombre;
+        this.tipo = tipo;
+        this.precio = precio;
+        this.imagen = imagen;
+        this.stock = stock;
+        this.cantidad = 0;
+    }
+
+    agregarCantidad(cantidadDeseada){
+        this.cantidad = this.cantidad + cantidadDeseada;
+    }
+
+    descripcion(){
+        return "id: " + this.id + " - " + this.nombre + " (" + this.tipo + ") " + "precio: $" + this.precio + "\n";
+    }
+
+    descripcionParaCarrito(){
+        return "id: " + this.id + " - " + this.nombre + " (" + this.tipo + ") " + "precio: $" + this.precio + " cantidad: "+ this.cantidad + "\n";
+    }
+}
+
+class ProductoController{
+    constructor(){
+        this.listaProductos = [];
+    }
+
+    agregar(producto){
+        this.listaProductos.push(producto);
+    }
+
+    mostrar(){
+        let descripcionListaProductos =[];
+        this.listaProductos.forEach( producto => {
+            descripcionListaProductos = descripcionListaProductos + producto.descripcion();
+        })
+        return descripcionListaProductos;
+    }
+
+    buscarId(id){
+        return this.listaProductos.find(producto => producto.id == id);
+    }
+}
+
+class Carrito{
+    constructor(){
+        this.listaCarrito = [];
+    }
+
+    agregar(productoNuevo){
+        let existe = this.listaCarrito.some(producto => producto.id == productoNuevo.id);
+        if(!existe){
+            this.listaCarrito.push(productoNuevo);
+        }
+    }
+
+    mostrar(){
+        let descripcionListaCompra = "Carrito: \n\n"
+        this.listaCarrito.forEach( producto => {
+            descripcionListaCompra = descripcionListaCompra + producto.descripcionParaCarrito();
+        })
+        return descripcionListaCompra;
+    }
+
+    calcularTotal(){
+        return this.listaCarrito.reduce((total,producto) => total + producto.precio * producto.cantidad, 0);
+    }
+}
+
+const p1 = new Producto(1, "Vino Reserva", "Tinto", 2000, "./img/product1.jpg", 5);
+const p2 = new Producto(2, "Vino Premium", "Blanco", 1800, "./img/product1.jpg", 5);
+const p3 = new Producto(3, "Vino Elegante", "Rosado", 1500, "./img/product1.jpg", 5);
+const p4 = new Producto(4, "Vino Gran Enemigo", "Tinto", 4000, "./img/product1.jpg", 5);
+const p5 = new Producto(5, "Vino Durigutti Cabernet Franc", "Tinto", 3000, "./img/product1.jpg", 5);
+const p6 = new Producto(6, "Vino Durigutti Tempranillo", "Tinto", 3500, "./img/product1.jpg", 5);
+const p7 = new Producto(7, "Vino Chardonnay", "Blanco", 6000, "./img/product1.jpg", 5);
+const p8 = new Producto(8, "Vino Extra Premium", "Espumante", 8000, "./img/product1.jpg", 5);
+
+const carrito = new Carrito();
+const controladorP = new ProductoController();
+
+controladorP.agregar(p1);
+controladorP.agregar(p2);
+controladorP.agregar(p3);
+controladorP.agregar(p4);
+controladorP.agregar(p5);
+controladorP.agregar(p6);
+controladorP.agregar(p7);
+controladorP.agregar(p8);
+
+function simularCarrito() {
+    let respuesta;
+    do{
+        let id = Number(prompt("Listado de productos:\n" + controladorP.mostrar() + "\n\nIngrese el ID del producto que desea comprar:"));
+        const producto = controladorP.buscarId(id);
+        let cantidadDeseada = Number(prompt("Ingrese la cantidad que desea"));
+        producto.agregarCantidad(cantidadDeseada);
+        carrito.agregar(producto);
+        alert(carrito.mostrar());
+
+        respuesta = prompt("¿Desea finalizar la compra? (escriba 'SI' para finalizar)").toLowerCase();
+    }while(respuesta != "si")
+
+    let costoEnvio = calcularCostoEnvio();
+    let costoCompra = carrito.calcularTotal();
+    let costoTotal = costoEnvio + costoCompra;
+alert("El total es de: $" + costoTotal + "\n-Compra: $ " + costoCompra +"\n-Envío: $ " + costoEnvio);
+}
+
+const botonSimular = document.getElementById("boton-simular-carrito");
+botonSimular.addEventListener("click", simularCarrito);
